7863f8e60cd8de65754eba197e04381ee809cce5
gamblevore <theo@jeebox.org>
2019-05-02 11:21:03 +0100

    updated licence

--- Cpp/JB.cpp
+++ Cpp/JB.cpp
@@ -5084,7 +5084,7 @@ __lib__ int jb_shutdown() {
 }
 
 __lib__ int jb_version() {
-	return 2019050122;
+	return 2019050211;
 }
 
 __lib__ JB_String* jb_readfile(_cstring path, bool AllowMissingFile) {
